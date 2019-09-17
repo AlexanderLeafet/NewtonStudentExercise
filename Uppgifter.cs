c04f9667940e6ce375713cc56167c1f3a8accf5b
Alexander.Lovdahl <alexander.lovdahl@gmail.com>
2019-09-17 15:50:10 +0200

    Lagt upp heltäckande uppgiftsfil

---
+++ Uppgifter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace NewtonStudentExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*sequencePrograming.övning1 o1 = new sequencePrograming.övning1();
+            o1.PrintKvadraten();*/
+
+            /*sequencePrograming.övning2 o2 = new sequencePrograming.övning2();
+            o2.PrintLön();*/
+
+            /*sequencePrograming.övning3 o3 = new sequencePrograming.övning3();
+            o3.PrintTid();*/
+
+            /*sequencePrograming.övning5 o5 = new sequencePrograming.övning5();
+            o5.ConvertCurrency();*/
+
+
+            /*ifConditions.övning11 o11 = new ifConditions.övning11();
+            o11.PrintIsVal1Bigger();*/
+
+            /*ifConditions.övning12 o12 = new ifConditions.övning12();
+            o12.PrintVal1DoubleSize();*/
+
+            /*ifConditions.övning13 o13 = new ifConditions.övning13();
+            o13.PrintIsEven();*/
+
+            /*ifConditions.övning14 o14 = new ifConditions.övning14();
+            o14.PrintIsEvenElse();*/
+
+            /*ifConditions.övning15 o15 = new ifConditions.övning15();
+            o15.PrintCheckEven();*/
+
+            /*ifConditions.övning16 o16 = new ifConditions.övning16();
+            o16.PrintTotalPris();*/
+
+            /*ifConditions.övning17 o17 = new ifConditions.övning17();
+            o17.PrintSumma();*/
+
+            /*ifConditions.övning23 o23 = new ifConditions.övning23();
+            o23.PrintKvot();*/
+
+            /*ifConditions.övning29 o29 = new ifConditions.övning29();
+            o29.PrintTrainTime();*/
+
+            /*Console.WriteLine("Välj kategori i menyn");
+            Console.WriteLine("");
+            Console.WriteLine("1) IfConditions");
+            Console.WriteLine("2) loopsFor");
+            Console.WriteLine("3) loopsWhile");
+            Console.WriteLine("4) sequencePrograming");
+            int Meny = Convert.ToInt32(Console.ReadLine());
+
+            switch (Meny)
+            {
+                case 1:
+                    ifConditions = new ifConditions();
+                    o11.PrintIsVal1Bigger();*/
+
+            }
+
+
+        }
+    }
+
